7cc6d0e89763186b685a717c80141aa959344e86
Matthew Daly <matthewd673@outlook.com>
2021-06-24 20:48:20 -0400

    Add Camera

---
+++ IsoEngine/Camera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IsoEngine
+{
+    public class Camera
+    {
+
+        public Vec2 pos;
+        int w;
+        int h;
+
+        public Camera(int w, int h)
+        {
+            this.pos = new Vec2(0, 0);
+            this.w = w;
+            this.h = h;
+        }
+
+        public Camera(Vec2 pos, int w, int h)
+        {
+            this.pos = pos;
+            this.w = w;
+            this.h = h;
+        }
+
+        public void CenterOnEntity(Entity e)
+        {
+            pos.x = (e.pos.x + e.w / 2) - w / Renderer.scale / 2;
+            pos.y = (e.pos.y + e.h / 2) - h / Renderer.scale / 2;
+        }
+
+        public Vec2 ScreenToWorldPos(float x, float y)
+        {
+            return new Vec2((x - pos.x) / Renderer.scale, (y - pos.y) / Renderer.scale);
+        }
+
+        public Vec2 ScreenToWorldPos(Vec2 screenPos)
+        {
+            return ScreenToWorldPos(screenPos.x, screenPos.y);
+        }
+
+        public Vec2 MouseToWorldPos()
+        {
+            return ScreenToWorldPos(InputHandler.mX, InputHandler.mY);
+        }
+
+    }
+}
--- IsoEngine/Renderer.cs
+++ IsoEngine/Renderer.cs
@@ -7,6 +7,7 @@ namespace IsoEngine
     public static class Renderer
     {
 
+        public static Camera cam = new Camera(new Vec2(0, 0), 800, 600); //default, should be changed based on preferredbackbufferwidth & height
         public static int scale = 2;
 
         public static void Render(EntityManager entityManager, SpriteBatch spriteBatch)
@@ -21,7 +22,7 @@ namespace IsoEngine
 
         public static Rectangle GetRenderBounds(Entity e)
         {
-            return new Rectangle((int)(e.pos.x * scale), (int)(e.pos.y * scale), e.w * scale, e.w * scale);
+            return new Rectangle((int)((e.pos.x - cam.pos.x) * scale), (int)((e.pos.y - cam.pos.y) * scale), e.w * scale, e.w * scale);
         }
 
     }
