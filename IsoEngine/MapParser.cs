f48a8efb29127e14a1ef243c58efb045e429e52d
Matthew Daly <matthewd673@outlook.com>
2021-07-06 21:40:26 -0400

    Add MapParser for MapTools files

---
+++ IsoEngine/MapParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IsoEngine
+{
+    public static class MapParser
+    {
+
+        /// <summary>
+        /// Given the contents of a MapTools map file, returns a 2D array of characters to be further parsed into Entities.
+        /// </summary>
+        /// <param name="data">The data of the map file.</param>
+        /// <returns>A 2D array of the map characters.</returns>
+        public static char[,] Parse(string data)
+        {
+            string[] lines = data.Split('\n');
+
+            string[] firstSplit = lines[0].Split(',');
+
+            int width = Convert.ToInt32(firstSplit[0]);
+            int height = Convert.ToInt32(firstSplit[1]);
+            char[,] map = new char[width, height];
+
+            int i = 0;
+            int j = 0;
+            foreach (char c in lines[1].ToCharArray())
+            {
+                map[i, j] = c;
+
+                i++;
+                if (i == width)
+                {
+                    i = 0;
+                    j++;
+                }
+            }
+
+            return map;
+        }
+
+    }
+}
