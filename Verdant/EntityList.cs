f74263694d0c6e350b1fb83e06ec9822d09f1af2
Matthew Daly <matthewd673@outlook.com>
2023-12-27 00:36:25 -0500

    Update GetCellRange to use iterator
    Formatting updates for EntityTable and EntityList

--- Verdant/EntityList.cs
+++ Verdant/EntityList.cs
@@ -3,47 +3,46 @@ using System.Collections.Generic;
 
 using Verdant.Physics;
 
-namespace Verdant
+namespace Verdant;
+
+internal class EntityList
 {
-    internal class EntityList
+
+    // TODO: in the future, hash this further for lookup by type
+    private List<Entity> entities;
+    private List<PhysicsEntity> physicsEntities;
+
+    public EntityList()
     {
+        entities = new();
+        physicsEntities = new();
+    }
 
-        // TODO: in the future, hash this further for lookup by type
-        private List<Entity> entities;
-        private List<PhysicsEntity> physicsEntities;
+    public void Add(Entity e)
+    {
+        if (e.IsType<PhysicsEntity>())
+            physicsEntities.Add((PhysicsEntity)e);
 
-        public EntityList()
-        {
-            entities = new();
-            physicsEntities = new();
-        }
+        entities.Add(e);
+    }
 
-        public void Add(Entity e)
-        {
-            if (e.IsType(typeof(PhysicsEntity)))
-                physicsEntities.Add((PhysicsEntity)e);
+    public bool Remove(Entity e)
+    {
+        // this is a little weird
+        if (e.IsType<PhysicsEntity>())
+            physicsEntities.Remove((PhysicsEntity)e);
 
-            entities.Add(e);
-        }
+        return entities.Remove(e);
+    }
 
-        public bool Remove(Entity e)
-        {
-            // this is a little weird
-            if (e.IsType(typeof(PhysicsEntity)))
-                physicsEntities.Remove((PhysicsEntity)e);
+    public List<Entity> GetEntities()
+    {
+        return entities;
+    }
 
-            return entities.Remove(e);
-        }
-
-        public List<Entity> GetEntities()
-        {
-            return entities;
-        }
-
-        public List<PhysicsEntity> GetPhysicsEntities()
-        {
-            return physicsEntities;
-        }
+    public List<PhysicsEntity> GetPhysicsEntities()
+    {
+        return physicsEntities;
     }
 }
 
--- Verdant/EntityTable.cs
+++ Verdant/EntityTable.cs
@@ -1,81 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Verdant
+namespace Verdant;
+
+internal class EntityTable
 {
-    internal class EntityTable
+    private Dictionary<ulong, EntityList> table;
+
+    public EntityTable()
     {
-        private Dictionary<ulong, EntityList> table;
+        table = new();
+    }
 
-        public EntityTable()
+    public void Insert(int x, int y, Entity e)
+    {
+        EntityList cell;
+        ulong key = Hash(x, y);
+
+        if (table.TryGetValue(key, out cell))
         {
-            table = new();
+            cell.Add(e);
+        }
+        else
+        {
+            EntityList l = new();
+            l.Add(e);
+            table.Add(key, l);
+        }
+    }
+
+    public bool Remove(int x, int y, Entity e)
+    {
+        EntityList cell;
+        if (table.TryGetValue(Hash(x, y), out cell))
+        {
+            return cell.Remove(e);
         }
 
-        public void Insert(int x, int y, Entity e)
-        {
-            EntityList cell;
-            ulong key = Hash(x, y);
+        return false;
+    }
 
-            if (table.TryGetValue(key, out cell))
+    public bool GetCell(int x, int y, out EntityList cell)
+    {
+        return table.TryGetValue(Hash(x, y), out cell);
+    }
+
+    public bool ContainsCell(int x, int y)
+    {
+        return table.ContainsKey(Hash(x, y));
+    }
+
+    public IEnumerable<EntityList> GetCellRange(int x1,
+                                                int y1,
+                                                int x2,
+                                                int y2
+        )
+    {
+        // iterate through all cells in range
+        for (int i = x1; i <= x2; i++)
+        {
+            for (int j = y1; j <= y2; j++)
             {
-                cell.Add(e);
-            }
-            else
-            {
-                EntityList l = new();
-                l.Add(e);
-                table.Add(key, l);
-            }
-        }
-
-        public bool Remove(int x, int y, Entity e)
-        {
-            EntityList cell;
-            if (table.TryGetValue(Hash(x, y), out cell))
-            {
-                return cell.Remove(e);
-            }
-
-            return false;
-        }
-
-        public bool GetCell(int x, int y, out EntityList cell)
-        {
-            return table.TryGetValue(Hash(x, y), out cell);
-        }
-
-        public bool ContainsCell(int x, int y)
-        {
-            return table.ContainsKey(Hash(x, y));
-        }
-
-        public EntityList[] GetCellRange(int x1, int y1, int x2, int y2)
-        {
-            EntityList[] output = new EntityList[Math.Abs((x2 - x1 + 1) * (y2 - y1 + 1))];
-            int p = 0;
-            for (int i = x1; i <= x2; i++)
-            {
-                for (int j = y1; j <= y2; j++)
+                EntityList l;
+                if (!GetCell(i, j, out l))
                 {
-                    if (!GetCell(i, j, out output[p]))
-                    {
-                        output[p] = new();
-                    }
-                    p++;
+                    continue;
                 }
+                yield return l;
             }
-            return output;
         }
+    }
 
-        private ulong Hash(int x, int y)
-        {
-            ulong key = 0x0;
-            key = key | (uint)x;
-            key = key << 32;
-            key = key | (uint)y;
-            return key;
-        }
+    private ulong Hash(int x, int y)
+    {
+        ulong key = 0x0;
+        key = key | (uint)x;
+        key = key << 32;
+        key = key | (uint)y;
+        return key;
     }
 }
 
